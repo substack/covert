6307727262cd2e7821f082e904de11675b8181c0
James Halliday <mail@substack.net>
2013-12-21 10:52:59 -0800

    drop --no-detect-globals with the latest browserify --bare

--- bin/cmd.js
+++ bin/cmd.js
@@ -35,7 +35,7 @@ var coverifyPath = require.resolve('coverify');
 try { coverifyPath = resolve.sync('coverify', { basedir: process.cwd() }) }
 catch (e) {}
 
-args.unshift('--no-detect-globals', '-t', coverifyPath, '--bare');
+args.unshift('-t', coverifyPath, '--bare');
 
 var browserify = spawn('browserify', args);
 browserify.stderr.pipe(process.stderr);
